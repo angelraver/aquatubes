6b8e50988b212c6bd3d29b5d83cac47540dcb0ed
angel <angeldavidflorentin@gmail.com>
2025-09-07 22:57:46 -0300

    animated background

---
+++ main/sprites/scenario/background_menu.go
@@ -0,0 +1,10 @@
+components {
+  id: "background_menu"
+  component: "/scripts/background_menu.script"
+}
+embedded_components {
+  id: "tile_factory"
+  type: "factory"
+  data: "prototype: \"/main/sprites/scenario/background_tile.go\"\n"
+  ""
+}
