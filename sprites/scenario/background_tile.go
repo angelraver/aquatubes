35fd40621270580385e83ecf142bcfd6ad8dedad
angel <angeldavidflorentin@gmail.com>
2025-09-22 23:42:04 -0300

    adding confetti - refactor

---
+++ sprites/scenario/background_tile.go
@@ -0,0 +1,11 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"menu_background\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/sprites/ui.atlas\"\n"
+  "}\n"
+  ""
+}
---
+++ sprites/scenario/tank.go
@@ -0,0 +1,15 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"tanque\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/sprites/sprites.atlas\"\n"
+  "}\n"
+  ""
+  scale {
+    x: 0.75
+    y: 0.75
+  }
+}
